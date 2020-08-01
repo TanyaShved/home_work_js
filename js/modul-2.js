1a76424ab39e16fe7a966aa0f00d4dda81e65a5a
Tatiana Shved <tanya.shved.89@gmail.com>
2020-08-01 14:11:52 +0300

    добавляет 2 и 3 задачи

--- js/modul-2.js
+++ js/modul-2.js
@@ -6,35 +6,99 @@
 // Нумерация должна начинаться с 1. К примеру для первого элемента массива ['Mango', 'Poly', 'Ajax'] с индексом 0 будет выведено '1 - Mango', а для индекса 2 выведет '3 - Ajax'.
 
 // Используйте вспомогательную переменную result для добавления (конкатенации) строк внутри цикла for
-const getItemsString = function(array) {
-//     // Write code under this line
-let result = "";
-for (let i = 0; i < array.length; i += 1) {
-  result += `${i+1} - ${array[i]}/n`;
-} 
-return result; 
-};
 
+// const getItemsString = function (array) {
+//   //     // Write code under this line
+//   let result = '';
+//   for (let i = 0; i < array.length; i += 1) {
+//     result += `${i + 1} - ${array[i]}\n`;
+//   }
+//   return result;
+// };
 
-  console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
-  
-  //console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
-  /*
-  '1 - Mango
-  2 - Poly
-  3 - Ajax
-  4 - Lux
-  5 - Jay
-  6 - Kong
-  '
-  */
-  
-  console.log(getItemsString([5, 10, 15]));
+// console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
 
-  //console.log(getItemsString([5, 10, 15]));
-  /*
-  '1 - 5
-  2 - 10
-  3 - 15
-  '
-  */
+// //console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
+// /*
+//   '1 - Mango
+//   2 - Poly
+//   3 - Ajax
+//   4 - Lux
+//   5 - Jay
+//   6 - Kong
+//   '
+//   */
+
+// console.log(getItemsString([5, 10, 15]));
+
+// //console.log(getItemsString([5, 10, 15]));
+// /*
+//   '1 - 5
+//   2 - 10
+//   3 - 15
+//   '
+//   */
+
+//
+
+// Задача 2
+
+// Подсчет стоимости гравировки украшений
+// Напиши скрипт подсчета стоимости гравировки украшений. Для этого создай функцию calculateEngravingPrice(message = "", pricePerWord = 0) принимающую строку (в строке будут только слова и пробелы) и цену гравировки одного слова, и возвращающую цену гравировки всех слов в строке.
+
+// Для решения этой задачи не используйте циклы. Т.е. никаких for, while, do while, for of, for in, forEach или функциональных методов.
+
+// const calculateEngravingPrice = (message = '', pricePerWord = 0) =>
+//   (pricePerWord = message.split(' ').length * pricePerWord); // Write code in this line
+
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     10,
+//   ),
+// ); // 80
+
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     20,
+//   ),
+// ); // 160
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+// ); // 240
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+// ); // 120
+
+// console.log(calculateEngravingPrice('Uno', 100)); // 100
+
+// Задача 3
+
+// Поиск самого длинного слово в строке с пробелами
+// Напиши функцию findLongestWord(string = ""), которая принимает параметром произвольную строку (в строке будут только слова и пробелы) и возвращает самое длинное слово в этой строке.
+
+function findLongestWord(string = '') {
+  let longestWord = string[0];
+  let longerString = string.split(' ');
+
+  console.log(longerString);
+
+  for (let i = 0; i < longerString.length; i += 1) {
+    if (longerString[i].length > longestWord.length) {
+      longestWord = longerString[i];
+    }
+  }
+  return longestWord;
+}
+
+console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+// 'jumped'
+
+console.log(findLongestWord('Google do a roll'));
+// 'Google'
+
+console.log(findLongestWord('May the force be with you'));
+// 'force'
